c64b65cf89b89979e6db8bf669df7ce883dc248e
Toby Nott <tobynott80@gmail.com>
2023-03-26 17:38:26 +0100

    formatted component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,82 +1,82 @@
-<style>
-    :global(body) {
-  background-color: #001b2e;
-}
-
-h1 {
-  margin-top: 0px;
-  margin-bottom: 0px;
-}
-
-p {
-  margin: 0px;
-}
-.links {
-  padding-top: 10px;
-  display: flex;
-  justify-content: center;
-}
-
-* {
-  color: #ffefd3;
-  font-family: "Courier New", Courier, monospace;
-  text-align: center;
-  padding-top: 5px;
-  padding-bottom: 5px;
-}
-
-.projects{
-  margin: 10px;
-  padding: 10px;
-}
-
-h3{
-  margin: 0px;
-  padding: 0px;
-  font-size: 14px;
-}
-
-h2{
-  margin: 5px;
-  padding: 10px;
-  font-size: 20px;
-}
-
-
-
-hr.dashed {
-  border-top: 3px dashed #bbb;
-  border-bottom: 0px;
-  border-left: 0px;
-  border-right: 0px;
-}
-</style>
-
-<header>   
-    <h1>Toby Nott</h1>
-</header>   
+<header>
+  <h1>Toby Nott</h1>
+</header>
 <div id="pfp">
-    <img src="/pfp2.webp" alt="Toby Nott" width="200px" height="201px">
+  <img src="/pfp2.webp" alt="Toby Nott" width="200px" height="201px" />
 </div>
 
 <div class="links">
-<a href="https://www.linkedin.com/in/tobiah-nott-380439140/">LinkedIn</a> 
-<p> / </p>
-<a href="https://github.com/tobynott80">Github</a> 
-<p> / </p>
-<a href="https://gitlab.com/tobynott80">GitLab</a> 
-<p> / </p>
-<a href="eth.html">ETH</a>
-<p> / </p>
-<a href="btc.html">BTC</a>
-
+  <a href="https://www.linkedin.com/in/tobiah-nott-380439140/">LinkedIn</a>
+  <p>/</p>
+  <a href="https://github.com/tobynott80">Github</a>
+  <p>/</p>
+  <a href="https://gitlab.com/tobynott80">GitLab</a>
+  <p>/</p>
+  <a href="eth.html">ETH</a>
+  <p>/</p>
+  <a href="btc.html">BTC</a>
 </div>
 <aside>
-    <p>Applied Software Engineering Undergrad @ Cardiff University</p>
+  <p>Applied Software Engineering Undergrad @ Cardiff University</p>
 </aside>
 
-<hr class="dashed">
+<hr class="dashed" />
 <h2>Projects</h2>
 <div class="projects">
-    <h3><a href="http://fmmp.toby.uk.eu.org/">Find My MP</a> - A simple flask site to find a MP from a postcode and gather related news.</h3>
+  <h3>
+    <a href="http://fmmp.toby.uk.eu.org/">Find My MP</a> - A simple flask site to
+    find a MP from a postcode and gather related news.
+  </h3>
 </div>
+
+<style>
+  :global(body) {
+    background-color: #001b2e;
+  }
+
+  h1 {
+    margin-top: 0px;
+    margin-bottom: 0px;
+  }
+
+  p {
+    margin: 0px;
+  }
+  .links {
+    padding-top: 10px;
+    display: flex;
+    justify-content: center;
+  }
+
+  * {
+    color: #ffefd3;
+    font-family: "Courier New", Courier, monospace;
+    text-align: center;
+    padding-top: 5px;
+    padding-bottom: 5px;
+  }
+
+  .projects {
+    margin: 10px;
+    padding: 10px;
+  }
+
+  h3 {
+    margin: 0px;
+    padding: 0px;
+    font-size: 14px;
+  }
+
+  h2 {
+    margin: 5px;
+    padding: 10px;
+    font-size: 20px;
+  }
+
+  hr.dashed {
+    border-top: 3px dashed #bbb;
+    border-bottom: 0px;
+    border-left: 0px;
+    border-right: 0px;
+  }
+</style>

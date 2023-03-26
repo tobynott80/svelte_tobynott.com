3666af6f481cd9d979f57b333b3fa0d04f8e73cc
Toby Nott <notttj@cardiff.ac.uk>
2023-03-26 17:37:31 +0100

    added css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,57 @@
+<style>
+    :global(body) {
+  background-color: #001b2e;
+}
+
+h1 {
+  margin-top: 0px;
+  margin-bottom: 0px;
+}
+
+p {
+  margin: 0px;
+}
+.links {
+  padding-top: 10px;
+  display: flex;
+  justify-content: center;
+}
+
+* {
+  color: #ffefd3;
+  font-family: "Courier New", Courier, monospace;
+  text-align: center;
+  padding-top: 5px;
+  padding-bottom: 5px;
+}
+
+.projects{
+  margin: 10px;
+  padding: 10px;
+}
+
+h3{
+  margin: 0px;
+  padding: 0px;
+  font-size: 14px;
+}
+
+h2{
+  margin: 5px;
+  padding: 10px;
+  font-size: 20px;
+}
+
+
+
+hr.dashed {
+  border-top: 3px dashed #bbb;
+  border-bottom: 0px;
+  border-left: 0px;
+  border-right: 0px;
+}
+</style>
+
 <header>   
     <h1>Toby Nott</h1>
 </header>   

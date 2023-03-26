02a2f97f0db1de6b7c657ad1b80e9d7733b75eda
Toby Nott <notttj@cardiff.ac.uk>
2023-03-26 17:22:40 +0100

    added old html

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<header>   
+    <h1>Toby Nott</h1>
+</header>   
+<div id="pfp">
+    <img src="/pfp2.webp" alt="Toby Nott" width="200px" height="201px">
+</div>
+
+<div class="links">
+<a href="https://www.linkedin.com/in/tobiah-nott-380439140/">LinkedIn</a> 
+<p> / </p>
+<a href="https://github.com/tobynott80">Github</a> 
+<p> / </p>
+<a href="https://gitlab.com/tobynott80">GitLab</a> 
+<p> / </p>
+<a href="eth.html">ETH</a>
+<p> / </p>
+<a href="btc.html">BTC</a>
+
+</div>
+<aside>
+    <p>Applied Software Engineering Undergrad @ Cardiff University</p>
+</aside>
+
+<hr class="dashed">
+<h2>Projects</h2>
+<div class="projects">
+    <h3><a href="http://fmmp.toby.uk.eu.org/">Find My MP</a> - A simple flask site to find a MP from a postcode and gather related news.</h3>
+</div>
+<!-- Simple Analytics Script -->
+<script async defer src="https://api.tobynott.com/latest.js"></script>
+<noscript><img src="https://api.tobynott.com/noscript.gif" alt="" referrerpolicy="no-referrer-when-downgrade" /></noscript>

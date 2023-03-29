bb7440bc651a62c30011d4d27d62ab924b74fc67
Toby Nott <notttj@cardiff.ac.uk>
2023-03-29 20:42:46 +0100

    added tools page

---
+++ src/routes/tools/+page.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+    <title>Toby Nott - Tools</title>
+</svelte:head>
+
+<h1>Tools</h1>
+
+<p>A selection of simple tools build with <a href="https://svelte.dev/">Svelte</a> with native JavaScript</p>
+
+<h2><a href="tools/base64">Base64 Encoder/Decoder</a></h2>
+<p>A simple tool to decode and encode Base64 data. <a href="">Source</a></p>

13310a2a07da417c4a330a2447b4401ce3f5dfb3
Toby Nott <notttj@cardiff.ac.uk>
2023-03-29 20:56:44 +0100

    added back button

--- src/routes/tools/+page.svelte
+++ src/routes/tools/+page.svelte
@@ -1,10 +1,34 @@
 <svelte:head>
-    <title>Toby Nott - Tools</title>
+  <title>Toby Nott - Tools</title>
 </svelte:head>
 
 <h1>Tools</h1>
 
-<p>A selection of simple tools build with <a href="https://svelte.dev/">Svelte</a> with native JavaScript</p>
+<p>
+  A selection of simple tools build with <a href="https://svelte.dev/">Svelte</a
+  > with native JavaScript
+</p>
+
+<hr class="dashed" />
 
 <h2><a href="tools/base64">Base64 Encoder/Decoder</a></h2>
-<p>A simple tool to decode and encode Base64 data. <a href="">Source</a></p>
+<p>
+  A simple tool to decode and encode Base64 data. 
+</p>
+<p><a href="https://github.com/tobynott80/svelte_tobynott.com/blob/dev/src/routes/tools/base64/%2Bpage.svelte">Source</a></p>
+
+<h2><a href="tools/passgen">Password Generator</a></h2>
+<p>
+  A random password generator with options for length, case and symbols,
+  ensuring <a href="https://support.microsoft.com/en-us/windows/create-and-use-strong-passwords-c5cebb49-8c53-4f5e-2bc4-fe357ca048eb">good practice</a>. 
+</p>
+<p><a href="https://github.com/tobynott80/svelte_tobynott.com/blob/dev/src/routes/tools/passgen/%2Bpage.svelte">Source</a></p>
+<p></p>
+<a href="/">&lt-- Back</a>
+<style>
+    p{
+        margin-left: 10%;
+        margin-right: 10%;
+        text-align: center;
+    }
+</style>

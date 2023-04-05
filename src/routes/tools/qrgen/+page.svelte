8b9d0e628ca6e119e6ecb152a99648e00f958865
Toby Nott <notttj@cardiff.ac.uk>
2023-04-05 21:28:08 +0100

    added initial page

---
+++ src/routes/tools/qrgen/+page.svelte
@@ -0,0 +1,15 @@
+<!--QR code generator-->
+<script>
+  import Page from "../../+page.svelte";
+
+
+</script>
+
+<svelte:head>
+    <title>Toby's QR Code Generator</title>
+    <meta name="description" content="Create QR codes for URLs and Text. Fast, free and open-source">
+</svelte:head>
+
+<h1>Toby's QR Code Generator</h1>
+
+<p>Generate a QR code from a URL or Text</p>

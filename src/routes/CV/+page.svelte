cd650d0afef7b0353be5924e4ee18b3f5f5750f5
Toby Nott <notttj@cardiff.ac.uk>
2023-10-18 08:56:47 +0100

    Update +page.svelte

--- src/routes/CV/+page.svelte
+++ src/routes/CV/+page.svelte
@@ -1,1 +1,7 @@
+<svelte:head>
+  <title>Toby Nott - CV</title>
+  <meta name="description" content="Tobiah Nott CV">
+
+</svelte:head>
+<h1>Download CV</h1>
 <a href="/TobiahNottCV.pdf" target="_blank">Download PDF</a>

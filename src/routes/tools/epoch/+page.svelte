c1dc369cd4062c855430dd74c885b01a19c2874b
Tobiah Nott <tobynott80@gmail.com>
2024-03-16 21:55:03 +0000

    Add Unix Epoch Converter component

---
+++ src/routes/tools/epoch/+page.svelte
@@ -0,0 +1,111 @@
+<script>
+  let DateInput = "";
+  let EpochInput = "";
+  let outputType = "seconds";
+  let inputType = "seconds";
+  let OutputEpochValue = "";
+  let OutputDateValue = "";
+
+  function convertDate() {
+    const dateValue = new Date(DateInput);
+    if (!isNaN(dateValue.getTime())) {
+      if (outputType === "timestamp") {
+        OutputEpochValue = dateValue.getTime().toString();
+      } else {
+        OutputEpochValue = Math.floor(dateValue.getTime() / 1000).toString();
+      }
+    } else {
+      OutputEpochValue = "Invalid date input";
+    }
+  }
+  function convertEpoch() {
+    const epochValue = parseInt(EpochInput);
+    if (inputType === "timestamp") {
+      if (epochValue.toString().length === 13) {
+        OutputDateValue = new Date(epochValue).toISOString();
+      } else {
+        OutputDateValue = "Invalid timestamp input";
+      }
+    } else {
+      if (epochValue.toString().length === 10) {
+        OutputDateValue = new Date(epochValue * 1000).toISOString();
+      } else {
+        OutputDateValue = "Invalid timestamp input";
+      }
+    }
+  }
+</script>
+
+<h1>Unix Epoch Converter</h1>
+<h2><u>Date → Epoch</u></h2>
+<h3>Convert a date time to a Unix timestamp</h3>
+<div>
+  <label>
+    Date Input:
+    <input
+      type="datetime-local"
+      bind:value={DateInput}
+      on:input={convertDate}
+    />
+  </label>
+</div>
+<div>
+  <label>
+    Output Type:
+    <select bind:value={outputType} on:change={convertDate}>
+      <option value="timestamp">Timestamp (milliseconds)</option>
+      <option value="seconds">Unix Timestamp (seconds)</option>
+    </select>
+  </label>
+</div>
+<div>
+  <label>
+    Output Value:
+    <input type="text" readonly value={OutputEpochValue} />
+  </label>
+</div>
+
+<hr class="dashed" />
+
+<h2><u>Epoch → Date</u></h2>
+<h3>Convert a Unix timestamp to a date</h3>
+<div>
+  <label>
+    Epoch Input:
+    <input type="number" bind:value={EpochInput} on:input={convertEpoch} />
+  </label>
+</div>
+<div>
+  <label>
+    Input Type:
+    <select bind:value={inputType} on:change={convertEpoch}>
+      <option value="timestamp">Timestamp (milliseconds)</option>
+      <option value="seconds">Unix Timestamp (seconds)</option>
+    </select>
+  </label>
+</div>
+<div>
+  <label>
+    Output Date:
+    <input type="text" readonly value={OutputDateValue} />
+  </label>
+</div>
+
+<style>
+  input,
+  select,
+  option {
+    margin: 5px;
+    color: black;
+  }
+  label {
+    width: 0px;
+    text-align: left;
+  }
+  h1 {
+    margin: 10px;
+  }
+  h2 {
+    font-size: 1em;
+  }
+</style>

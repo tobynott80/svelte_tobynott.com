898f2ce9ef82fce953ffd399142a8a4d915b7060
Tobiah Nott <tobynott80@gmail.com>
2024-03-16 21:57:22 +0000

    added epoch converter to tools page

--- src/routes/tools/+page.svelte
+++ src/routes/tools/+page.svelte
@@ -1,7 +1,9 @@
 <svelte:head>
   <title>Toby Nott - Tools</title>
-  <meta name="description" content="Free tools provided by Toby Nott. Fast, Free and Open Source">
-
+  <meta
+    name="description"
+    content="Free tools provided by Toby Nott. Fast, Free and Open Source"
+  />
 </svelte:head>
 
 <h1>Tools</h1>
@@ -14,33 +16,53 @@
 <hr class="dashed" />
 
 <h2><a href="tools/base64">Base64 Encoder/Decoder</a></h2>
+<p>A simple tool to decode and encode Base64 data.</p>
 <p>
-  A simple tool to decode and encode Base64 data. 
+  <a
+    href="https://github.com/tobynott80/svelte_tobynott.com/blob/dev/src/routes/tools/base64/%2Bpage.svelte"
+    >Source</a
+  >
 </p>
-<p><a href="https://github.com/tobynott80/svelte_tobynott.com/blob/dev/src/routes/tools/base64/%2Bpage.svelte">Source</a></p>
 
 <h2><a href="tools/passgen">Password Generator</a></h2>
 <p>
   A random password generator with options for length, case and symbols,
-  ensuring <a href="https://support.microsoft.com/en-us/windows/create-and-use-strong-passwords-c5cebb49-8c53-4f5e-2bc4-fe357ca048eb">good practice</a>. 
+  ensuring <a
+    href="https://support.microsoft.com/en-us/windows/create-and-use-strong-passwords-c5cebb49-8c53-4f5e-2bc4-fe357ca048eb"
+    >good practice</a
+  >.
 </p>
-<p><a href="https://github.com/tobynott80/svelte_tobynott.com/blob/dev/src/routes/tools/passgen/%2Bpage.svelte">Source</a></p>
+<p>
+  <a
+    href="https://github.com/tobynott80/svelte_tobynott.com/blob/dev/src/routes/tools/passgen/%2Bpage.svelte"
+    >Source</a
+  >
+</p>
+
+<h2><a href="tools/epoch">Unix Epoch Converter</a></h2>
+<p>A simple tool to convert a date time to a Unix timestamp.</p>
+<p>
+  <a
+    href="https://github.com/tobynott80/svelte_tobynott.com/blob/dev/src/routes/tools/passgen/%2Bpage.svelte"
+    >Source</a
+  >
+</p>
 
 <hr class="dashed" />
 
 <h3><a href="/">Toby Nott</a> / <a href="/tools">Tools</a></h3>
 
 <style>
-    p{
-        margin-left: 10%;
-        margin-right: 10%;
-        text-align: center;
-    }
+  p {
+    margin-left: 10%;
+    margin-right: 10%;
+    text-align: center;
+  }
 
-    h2{
-        color: #FFF0D6;
-    }
-    h3{
-      margin: 10px;
-    }
+  h2 {
+    color: #fff0d6;
+  }
+  h3 {
+    margin: 10px;
+  }
 </style>

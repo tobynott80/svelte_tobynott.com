28112a34764d0cc9c3a7cd12ac5de1745c7e5666
Toby Nott <tn@deliowealth.com>
2026-01-12 14:28:29 +0000

    dynamic year

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,62 +1,62 @@
 <svelte:head>
-  <title>Toby Nott</title>
-  <meta
-    name="description"
-    content="Toby Nott. Applied Software Engineer @ Cardiff University."
-  />
+    <title>Toby Nott</title>
+    <meta
+            name="description"
+            content="Toby Nott. Applied Software Engineer @ Cardiff University."
+    />
 </svelte:head>
 
+<script>
+    const currentYear = new Date().getFullYear();
+</script>
+
 <header>
-  <h1>Toby Nott</h1>
+    <h1>Toby Nott</h1>
 </header>
 <div id="pfp">
-  <img src="/pfp3.jpg" alt="Toby Nott" width="200px" height="201px" />
+    <img src="/pfp3.jpg" alt="Toby Nott" width="200px" height="201px"/>
 </div>
 
 <div class="links">
-  <a href="https://www.linkedin.com/in/tobiah-nott-380439140/" class="link"
+    <a href="https://www.linkedin.com/in/tobiah-nott-380439140/" class="link"
     >LinkedIn</a
-  >
-  <p>/</p>
-  <a href="https://github.com/tobynott80" class="link">Github</a>
-  <p>/</p>
-  <a href="https://gitlab.com/tobynott80" class="link">GitLab</a>
+    >
+    <p>/</p>
+    <a href="https://github.com/tobynott80" class="link">Github</a>
+    <p>/</p>
+    <a href="https://gitlab.com/tobynott80" class="link">GitLab</a>
 </div>
-<!-- <aside>
-  <p>Bridging the gap between protected data and valuable insights @ <a href="https://pontiro.com/">Pontiro</a></p>
-</aside> -->
-
-<hr class="dashed" />
+<hr class="dashed"/>
 <h2>Projects</h2>
 <div class="projects">
-  <h3>
-    <a href="https://heatwise.tobynott.com/">HeatWise</a> - A Next.js web app to visualise
-    spatial-temporal heat demand across England and Wales.
-  </h3>
-  <h3>
-    <a
-      href="https://install.appcenter.ms/users/tobynott/apps/mycommute/distribution_groups/test%20group"
-      >My Commute</a
-    > - A simple Android app to help you plan your commute.
-  </h3>
-  <h3>
-    <a href="https://github.com/GrLdev/Rate-My-Student-Home"
-      >Rate My Student Home</a
-    > - A website designed to let students rate their student homes in Cardiff (Work
-    in Progress).
-  </h3>
-  <h3>
-    <a href="tools/">Tools</a> - A selection of simple tools built with Svelte and
-    JavaScript.
-  </h3>
+    <h3>
+        <a href="https://heatwise.tobynott.com/">HeatWise</a> - A Next.js web app to visualise
+        spatial-temporal heat demand across England and Wales.
+    </h3>
+    <h3>
+        <a
+                href="https://install.appcenter.ms/users/tobynott/apps/mycommute/distribution_groups/test%20group"
+        >My Commute</a
+        > - A simple Android app to help you plan your commute.
+    </h3>
+    <h3>
+        <a href="https://github.com/GrLdev/Rate-My-Student-Home"
+        >Rate My Student Home</a
+        > - A website designed to let students rate their student homes in Cardiff (Work
+        in Progress).
+    </h3>
+    <h3>
+        <a href="tools/">Tools</a> - A selection of simple tools built with Svelte and
+        JavaScript.
+    </h3>
 </div>
 
-<hr class="dashed" />
+<hr class="dashed"/>
 
-<h3>Toby Nott - 2025</h3>
+<h3>Toby Nott - {currentYear}</h3>
 
 <style>
-  h3 {
-    margin-bottom: 10px;
-  }
+    h3 {
+        margin-bottom: 10px;
+    }
 </style>
